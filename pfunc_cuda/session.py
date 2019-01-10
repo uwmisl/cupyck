28fa9ec67628ace2415e130b2ff63f60f3fbc712
Callista Bee <callista.lavender.bee@gmail.com>
2019-01-10 10:40:13 -0800

    factor out options validation / add nvidia check

--- pfunc_cuda/session.py
+++ pfunc_cuda/session.py
@@ -1,34 +1,81 @@
 import ctypes
 import os
 import math
+import subprocess
+import xml.etree.ElementTree as et
 
 DNA = 0
 RNA = 1
+
+class Options(object):
+    def __init__(self, max_seqlen, **kwargs):
+
+        options = {
+            "nblocks"    : None,
+            "nthreads"   : 64,
+            "t_lo"       : 0,
+            "t_hi"       : 100,
+            "t_step"     : 0.05,
+            "na"         : 1.0,
+            "mg"         : 0.0,
+            "long_helix" : 1,
+            "dangle_type": 1,
+            "material"   : DNA
+        }
+        options.update(kwargs)
+
+        self.max_seqlen = max_seqlen
+        for opt, val in options.items():
+            self.__setattr__(opt, val)
+
+        if self.max_seqlen <= 0:
+            raise ValueError("max. sequence length must be positive")
+
+        if self.t_hi < self.t_lo:
+            raise ValueError("hi temp cannot be less than lo temp")
+
+        if self.t_step <= 0:
+            raise ValueError("temp step must be positive")
+
+        if self.na < 0 or self.mg < 0:
+            raise ValueError("salt concentrations must be non-negative")
+
+        if self.long_helix not in [0,1]:
+            raise ValueError("invalid long helix specification")
+
+        if self.dangle_type not in [0,1,2]:
+            raise ValueError("invalid dangle type specification")
+
+        if self.material not in [RNA, DNA]:
+            raise ValueError("invalid material specification")
+
+        if self.nblocks is None:
+            gpu_info = et.fromstring(
+                subprocess.check_output(
+                    ["nvidia-smi", "-i", "0", "-q", "-x"]
+                )
+            )
+
+            mem_free_text = gpu_info.find(".//fb_memory_usage/free").text
+            if "MiB" not in mem_free_text:
+                raise ValueError("unrecognized memory utilization type")
+
+            mem_free = int(mem_free_text.split(" ")[0]) * (1024 * 1024)
+
+            mem_needed = (
+                (max_seqlen ** 2) / 2 * # entries in each pf array
+                8                     * # bytes per entry
+                10                      # number of arrays
+            )
+
+            self.nblocks = min(mem_free / mem_needed, 65536)
+
 
 class Session(object):
 
     lib = None
 
-    def __init__(
-            self,
-            nblocks,
-            nthreads,
-            max_seqlen,
-            t_lo=0, t_hi=100, t_step=0.05,
-            na=1.0, mg=0.0,
-            long_helix=0,
-            dangle_type=1,
-            material=DNA
-            ):
-
-        if t_hi < t_lo:
-            raise ValueError("hi temp cannot be less than lo temp")
-
-        if t_step < 0:
-            raise ValueError("temp step must be positive")
-
-        if na < 0 or mg < 0:
-            raise ValueError("salt concentrations must be positive")
+    def __init__(self, options):
 
         package_dir = os.path.dirname(__file__)
         lib_dir = os.path.join(package_dir, "../lib")
@@ -39,17 +86,17 @@ class Session(object):
         )
 
         self.lib.pfuncInitialize(
-                ctypes.c_int(nblocks),
-                ctypes.c_int(nthreads),
-                ctypes.c_int(max_seqlen),
-                ctypes.c_double(t_lo),
-                ctypes.c_double(t_hi),
-                ctypes.c_double(t_step),
-                ctypes.c_double(na),
-                ctypes.c_double(mg),
-                ctypes.c_int(long_helix),
-                ctypes.c_int(dangle_type),
-                ctypes.c_int(material)
+                ctypes.c_int(options.nblocks),
+                ctypes.c_int(options.nthreads),
+                ctypes.c_int(options.max_seqlen),
+                ctypes.c_double(options.t_lo),
+                ctypes.c_double(options.t_hi),
+                ctypes.c_double(options.t_step),
+                ctypes.c_double(options.na),
+                ctypes.c_double(options.mg),
+                ctypes.c_int(options.long_helix),
+                ctypes.c_int(options.dangle_type),
+                ctypes.c_int(options.material)
                 )
 
 
